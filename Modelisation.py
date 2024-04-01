425bfe660023f861811c7a78979eb5daffd60d33
l3miage-moussola <adam.moussolna@etu.univ-grenoble-alpes.fr>
2024-04-01 19:43:53 +0200

    Création classes visite et tournée

---
+++ Modelisation.py
@@ -0,0 +1,10 @@
+
+
+def optimTempsVehicules():
+    visites = [] # liste de listes qui contient les ordonancements
+    nbJours = 0
+
+
+
+
+    return len(visites), nbJours
--- hello_word.py
+++ hello_word.py
@@ -5,10 +5,33 @@ not when you import the module.
 
 @author: vlehoux
 '''
+import csv
+from tournee import Tournee
 
-
+from visite import Visite
+def lire_visites_de_csv(nom_fichier):
+    visites = []
+    with open(nom_fichier, 'r') as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)  # Skip header
+        for row in csv_reader:
+            visit_id, visit_name, _, _, demand = row
+            # Convertir demand en entier
+            demand = int(demand)
+            # Instancier la classe Visite avec les données de chaque ligne
+            visite = Visite(visit_id, visit_name, demand)
+            visites.append(visite)
+    return visites
 def main():
-    print("Hello word!")
+    
+    mesVisites = lire_visites_de_csv("visits.csv")
+
+    print("Hello word!\n")
+    print(" visite 1 : " + str(mesVisites[1])) # mesVisites[0]
+
+    maTournee = Tournee(mesVisites[0:2])
+    for etape in maTournee.visites:
+        print(str(etape))
 
 
 if __name__ == '__main__':
